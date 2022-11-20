0afd8c968cbaf94db76e6a89f21a337d1fb40df2
vladislavkorolcuk <9329496@mail.ru>
2022-11-21 00:24:07 +0300

    Initial commit

---
+++ src/main/java/com/employee/lessontestingmockito/LessonTestingMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.employee.lessontestingmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTestingMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTestingMockitoApplication.class, args);
+    }
+
+}
